8960c576c66a5666af35745addb2de5f5aec6b8f
Jaime de Greiff <alegreiff@gmail.com>
2022-11-08 21:21:12 -0500

    misapoYados

--- pages/amigos/[id].js
+++ pages/amigos/[id].js
@@ -1,12 +1,39 @@
+import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function PollerosAMigosAdmin() {
   const router = useRouter();
   const { id } = router.query;
+
+  const [polleros, setPolleros] = useState([]);
+
+  useEffect(() => {
+    async function cargaMisPolleros() {
+      console.log("MIS", { id });
+      let { data, error } = await supabaseClient.rpc("mispolleros", {
+        pollero: id,
+      });
+
+      if (error) console.error("ERRATA", error);
+      else setPolleros(data);
+    }
+    cargaMisPolleros();
+  }, []);
+
   return (
     <>
       <h3>Polleros amigos {id}</h3>
-      <p>Proxiomamente administración de sus polleros</p>
+      <p>Proximamente administración de sus polleros</p>
+      {polleros &&
+        polleros.map((pollero) => (
+          <div key={pollero.userid}>
+            <p>
+              {" "}
+              {pollero.apodo} - {pollero.mail}{" "}
+            </p>
+          </div>
+        ))}
     </>
   );
 }
